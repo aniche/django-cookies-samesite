67c82489908845ce00d7d8811e56d72630dac795
tinytap <uri@tinytap.it>
2020-03-22 13:12:25 +0200

    add Secure tag

--- django_cookies_samesite/middleware.py
+++ django_cookies_samesite/middleware.py
@@ -81,9 +81,11 @@ class CookiesSameSite(MiddlewareMixin):
         if self.samesite_force_all:
             for cookie in response.cookies:
                 response.cookies[cookie]['samesite'] = self.samesite_flag
+                id self.samesite_flag == 'None': response.cookies[cookie]['Secure'] ='1'
         else:
             for cookie in self.protected_cookies:
                 if cookie in response.cookies:
                     response.cookies[cookie]['samesite'] = self.samesite_flag
+                    id self.samesite_flag == 'None': response.cookies[cookie]['Secure'] ='1'
 
         return response
